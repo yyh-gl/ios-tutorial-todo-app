114bd94d457fce7b4155ee01a73fcc05cf878e1d
yyh-gl <yhonda.95.gl@gmail.com>
2019-05-23 13:18:49 +0900

    リスト表示機能完成 & ToDo追加ボタン追加（表示のみ）

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController { 
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+        }
+    }
+    
+    private var tasks = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tasks = [
+            "abc", "efg", "hoge",
+            "hoge", "hoge", "hoge",
+            "hoge", "hoge", "hoge",
+            "hoge", "hoge", "hoge",
+            "hoge", "hoge", "hoge",
+            "hoge", "hoge", "hoge",
+            "hoge", "hoge", "hoge",
+            "ほんだ"
+        ]
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+}
